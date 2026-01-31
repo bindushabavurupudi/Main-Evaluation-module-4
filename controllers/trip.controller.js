64fd957aa020f63e4e209defdbf9561a3449cf1d
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-01-31 20:05:51 +0530

    added

---
+++ controllers/trip.controller.js
@@ -0,0 +1,34 @@
+import { supabase } from "../config/supabase.js";
+
+export const createTrip = async(req, res)=>{
+    const {customer_id, vehicle_id, distance_km, passengers} = req.body;
+     
+    const {data: vehicle} = await supabase.from("vehicles")
+    .select("*")
+    .eq("id", vehicle_id)
+    .single();
+
+    if(!vehicle.isAvailable)
+        return res.status(400).json({message: "Vehicle not available"});
+
+    if(passengers > vehicle.allowed_passengers)
+        return res.status(400).json({message: "Passenger limit exceeded"});
+
+    await supabase.from("vehicles").update({isAvailable: false}).eq("id", vehicle_id);
+    await supabase.from("trips").insert([{...req.bosy}]);
+
+    res.status(201).json({message:"Trip created"});
+}
+
+export const endTrip = async (req, res)=>{
+    const tripId = req.params;
+    const {data: trip} = await supabase.from("trips")
+    .select("distance_km, vehicle_id, vehicles(rate_per_km)")
+    .eq("id", tripId)
+    .single();
+
+    const cost = trip.distance_km * trip.vehicles.rate_per_km;
+
+    await 
+    
+}
